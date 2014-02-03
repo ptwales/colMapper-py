6c749212b6feb47acde567a2d29afdca73915e24
Philip Wales <ptwales@gmail.com>
2014-02-03 15:56:46 -0600

    Moving away from objects

--- mapOper.py
+++ mapOper.py
@@ -1,65 +1,33 @@
-class ImapOper(object):
+__FAIL__ = "~FAIL!"
+"""
+def __stripNULL__(valueList):
+    for value in valueList:
+        if value = NULL:
+            valueList.remove(value)
+""" 
+def mapIs(valueList):
+    return values[0]
 
-    def __init__(self):
-
-    def Evaluate(self, valueList):
-        print ("Evaluate not Initialized")
-
-    def getToken(self):
-        return self.token
-# operators do not touch the worksheets they are printing to
-# They are functional objects that are to be stacked, queued, listed, etc
-#    def __stdList__(self, mapCmd):
-#        for i in range(0, len(mapCmd)):
-#            for row in range(1, sheet.UsedRange.Rows.Count):
-#                values[i][row] = sheet.cells(row, mapCmd[i]).value
-#        return values
-
-
-class mapIs(ImapOper):
-
-    def __init__(self):
-        self.token = "="
-
-    def Evaluate(self, valueList):
-        return values[0]
-
-
-class mapSum(ImapOper):
+def mapSum(valueList):
+    return sum(valueList)
     
-    def __init__(self):
-        self.token = "+"
-
-    def Evaluate(self, valueList):
-        return sum(valueList)
-
-
-class mapAss(ImapOper):
-    
-    def __init__(self):
-        self.token = "=="
-        self.ASSERTFAIL = "~ASSERT_FAIL!"
-    
-    def Evaluate(self, valueList):
-        # Later Optionally remove Null Values from the list
-        # self.__stripNull__(valueList)
-        if len(valueList) == 1:
+def mapAss(valueList):
+    # Later Optionally remove Null Values from the list
+    # self.__stripNull__(valueList)
+    #__stripNull__(valueList)
+    if len(valueList) == 1:
             return valueList[0]
         for i in range(0, len(valueList) - 1):
             if valueList[i] != valueList[i+1]:
-                return self.ASSERTFAIL
+                return __FAIL__
         return valueList[0]
-    
-    def __stripNULL__(self, valueList):
-        # remove Null values from the list
 
+def mapCond(valueList):
+    # valueList = (BOOL, TRUE_VALUE, FALSE_VALUE)
+    assert len(valueList) = 3
+    return valueList[1] if valueList[0] else valueList[2]
 
-class mapCond(ImapOper):
-    
-    def __init__(self):
-        self.token = "?"
-    
-    def Evaluate(self, valueList):
-        # valueList = (BOOL, TRUE_VALUE, FALSE_VALUE)
-        assert len(valueList) = 3
-        return valueList[1] if valueList[0] else valueList[2]
+def mapProd(valueList):
+    #__stripNull__(valueList)
+    return reduce(operator.mul, iterable, 1)
+
