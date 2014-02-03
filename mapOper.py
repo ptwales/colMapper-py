918d8338b7f0d65981a04ee9b505a40aea600ac6
Philip Wales <ptwales@gmail.com>
2014-02-03 12:15:39 -0600

    Redesigned mapOper
    mapping Operators are functional classes that do not touch the worksheets.  They are meant to be contained functions. I almost would replace them with pure functions and have mapper have a container of delegates if it wasn't for the tokens.

--- mapOper.py
+++ mapOper.py
@@ -2,31 +2,64 @@ class ImapOper(object):
 
     def __init__(self):
 
-    def Evaluate(self, mapCmd):
+    def Evaluate(self, valueList):
         print ("Evaluate not Initialized")
 
     def getToken(self):
         return self.token
-
-    def __stdList__(self, mapCmd):
-        for i in range(0, len(mapCmd)):
-            for row in range(1, sheet.UsedRange.Rows.Count):
-                values[i][row] = sheet.cells(row, mapCmd[i]).value
-        return values
+# operators do not touch the worksheets they are printing to
+# They are functional objects that are to be stacked, queued, listed, etc
+#    def __stdList__(self, mapCmd):
+#        for i in range(0, len(mapCmd)):
+#            for row in range(1, sheet.UsedRange.Rows.Count):
+#                values[i][row] = sheet.cells(row, mapCmd[i]).value
+#        return values
 
 
 class mapIs(ImapOper):
 
-    def Evaluate(self, mapCmd):
-        return mapCmd[0]
+    def __init__(self):
+        self.token = "="
+
+    def Evaluate(self, valueList):
+        return values[0]
 
 
 class mapSum(ImapOper):
+    
+    def __init__(self):
+        self.token = "+"
 
-    def Evaluate(self, mapCmd):
-        row_i = 2
-        for row in self.__stdList__(mapCmd):
-            value = 0
-            for col in row
-                value = col + value
-                sheet.cells(row_i).value = value
+    def Evaluate(self, valueList):
+        return sum(valueList)
+
+
+class mapAss(ImapOper):
+    
+    def __init__(self):
+        self.token = "=="
+        self.ASSERTFAIL = "~ASSERT_FAIL!"
+    
+    def Evaluate(self, valueList):
+        # Later Optionally remove Null Values from the list
+        # self.__stripNull__(valueList)
+        if len(valueList) == 1:
+            return valueList[0]
+        for i in range(0, len(valueList) - 1):
+            if valueList[i] != valueList[i+1]:
+                return self.ASSERTFAIL
+        return valueList[0]
+    
+    def __stripNULL__(self, valueList):
+        # remove Null values from the list
+
+
+class mapCond(ImapOper):
+    
+    def __init__(self):
+        self.token = "?"
+    
+    def Evaluate(self, valueList):
+        # valueList = (BOOL, TRUE_VALUE, FALSE_VALUE)
+        assert len(valueList) = 3
+        return valueList[1] if valueList[0] else valueList[2]
