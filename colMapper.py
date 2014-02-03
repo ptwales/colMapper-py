a406bf339a2351737179a1cc5335f3310f2a7d39
Philip Wales <ptwales@gmail.com>
2014-02-03 12:59:01 -0600

    Mapped out how it should work in comments

--- colMapper.py
+++ colMapper.py
@@ -1,19 +1,46 @@
+# python modules for spreadsheet manipulating not loaded
+# CONTAINS PSEUDO CODE FOR IT
+# => pseudo code is VBA
 class colMapper(object):
-    
+
     def __init__(self):
-        # declare queue 
-
+        # declare dict
+        """
+        Use Dictionary or Queue?
+        
+        mapCmds: dict of lists of more lists or functions or parameters
+        first element in list is the function, rest are parameters (Polish/LISP)
+        
+        dict key is column to map to
+        Total evaluation of a dict element contains the map to a column
+        
+        for each element in dict (or queue
+            for each row
+                recurslively evaluate the list.
+                    first element is function,
+                    following are arguements
+                        if arguement is a list
+                            evaluate that list
+                        else arguement is a column
+                            get value of that column for current row.
+                returned value becomes the value of the (current row, dict key column)
+        """
+        
     # Set map commands
-    def setMapCmdWithString(self, stringCmd):
+    # Move this to new class
+    #def setMapCmdWithString(self, stringCmd):
 
-    def queueMapCmd(self, IMapOper):
+    def addMapCmd(self, IMapOper, destColumn):
     
-    def doMapCmd(self, IMapOper):
+    
+    def stackMapCmd(self, IMapOper):
+    
+    
 
     # Engine
-    def mapTo(self, fSheet, tSheet, topRow=2, bottomRow=0, toTopRow=2):
+    def mapTo(self, fromSheet, toSheet, topRow=2, bottomRow=0, toTopRow=2):
         if bottomRow < 1:
-            bottomRow = fSheet.UsedRange.Rows.Count
+            bottomRow = fromSheet.UsedRange.Rows.Count
         assert topRow < bottomRow and toTopRow > 0
         for row in range(topRow, bottomRow):
             for command in cmdQueue:
