8235d757fff2b33e3ec79cc19ab5742cc0759d77
Philip Wales <ptwales@gmail.com>
2014-02-03 14:42:44 -0600

    Update colMapper.py

--- colMapper.py
+++ colMapper.py
@@ -1,47 +1,48 @@
 # python modules for spreadsheet manipulating not loaded
 # CONTAINS PSEUDO CODE FOR IT
 # => pseudo code is VBA
-class colMapper(object):
+
+"""
+Use Dictionary or Queue?
+
+mapCmds: dict of lists of more lists or functions or parameters
+first element in list is the function, rest are parameters (Polish/LISP)
+
+dict key is column to map to
+Total evaluation of a dict element contains the map to a column
+
+for each element in dict (or queue
+    for each row
+        recurslively evaluate the list.
+            first element is function,
+            following are arguements
+                if arguement is a list
+                    evaluate that list
+                else arguement is a column
+                    get value of that column for current row.
+        returned value becomes the value of the (current row, dict key column)
+"""
+        
+class colMapCmd(object):
 
     def __init__(self):
         # declare dict
-        """
-        Use Dictionary or Queue?
-        
-        mapCmds: dict of lists of more lists or functions or parameters
-        first element in list is the function, rest are parameters (Polish/LISP)
-        
-        dict key is column to map to
-        Total evaluation of a dict element contains the map to a column
-        
-        for each element in dict (or queue
-            for each row
-                recurslively evaluate the list.
-                    first element is function,
-                    following are arguements
-                        if arguement is a list
-                            evaluate that list
-                        else arguement is a column
-                            get value of that column for current row.
-                returned value becomes the value of the (current row, dict key column)
-        """
-        
-    # Set map commands
-    # Move this to new class
-    #def setMapCmdWithString(self, stringCmd):
-
-    def addMapCmd(self, IMapOper, destColumn):
     
+    def addMapCmd(self, IMapOper, destColumn):
     
     def stackMapCmd(self, IMapOper):
     
     
+def colMap(mapCommands, fromSheet, toSheet, topRow=2, bottomRow=0, toTopRow=2):
+    if bottomRow < 1:
+        bottomRow = fromSheet.UsedRange.Rows.Count
+    assert topRow < bottomRow and toTopRow > 0
+    for row in range(topRow, bottomRow):
+        for key in mapCommands.keys():
+            toSheet.Cells(row, key).value = evaluateCommand(mapCommands[key], fromSheet)
 
-    # Engine
-    def mapTo(self, fromSheet, toSheet, topRow=2, bottomRow=0, toTopRow=2):
-        if bottomRow < 1:
-            bottomRow = fromSheet.UsedRange.Rows.Count
-        assert topRow < bottomRow and toTopRow > 0
-        for row in range(topRow, bottomRow):
-            for command in cmdQueue:
-                self.doMapCmd(command)
+def evaluateCommand(mapCmd, fromSheet):
+    # function is mapCmd[0]
+    # arguments are mapCmd[1-...]
+    # if arguments are not a list then they are a value from fromSheet
+                
