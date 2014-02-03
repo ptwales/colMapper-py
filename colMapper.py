ddc44f7bc5cb2bfc2fd3ddfc06d8346c318b71a5
ptwales <ptwales@gmail.com>
2014-02-02 22:18:45 -0600

    Syntax fixes

--- colMapper.py
+++ colMapper.py
@@ -1,18 +1,20 @@
 class colMapper(object):
-	# Set map commands
-	def setMapCmdWithString(self, stringCmd):
-		return
-	def pushMapCmd(IMapOper):
-		return
-	# Engine
-	def mapTo(fSheet, tSheet, topRow=2, bottomRow=0, toTopRow=2):
-		if bottomRow < 1:
-			bottomRow = fSheet.UsedRange.Rows.Count
-		assert topRow < bottomRow
-		assert toTopRow > 0
-		for row in range(topRow, bottomRow):
-			for command in mapStack:
-				
-		return
-	pass
-	
+    
+    def __init__(self):
+        # declare queue 
+
+    # Set map commands
+    def setMapCmdWithString(self, stringCmd):
+
+    def queueMapCmd(self, IMapOper):
+    
+    def doMapCmd(self, IMapOper):
+
+    # Engine
+    def mapTo(self, fSheet, tSheet, topRow=2, bottomRow=0, toTopRow=2):
+        if bottomRow < 1:
+            bottomRow = fSheet.UsedRange.Rows.Count
+        assert topRow < bottomRow and toTopRow > 0
+        for row in range(topRow, bottomRow):
+            for command in cmdQueue:
+                self.doMapCmd(command)
--- mapOper.py
+++ mapOper.py
@@ -1,28 +1,32 @@
 class ImapOper(object):
-	def Evaluate(mapCmd[]):
-		print "Evaluate not Initialized"
-		return
-	def getToken(self):
-		return self.token
-	def __stdList__(mapCmd[]):
-		for i in range(0, len(mapCmd))
-			for row in range(1, sheet.UsedRange.Rows.Count)
-				values[i][row] = sheet.cells(row,mapCmd[i]).value
-		return values
-	pass
+
+    def __init__(self):
+
+    def Evaluate(self, mapCmd):
+        print ("Evaluate not Initialized")
+
+    def getToken(self):
+        return self.token
+
+    def __stdList__(self, mapCmd):
+        for i in range(0, len(mapCmd)):
+            for row in range(1, sheet.UsedRange.Rows.Count):
+                values[i][row] = sheet.cells(row, mapCmd[i]).value
+        return values
+
 
 class mapIs(ImapOper):
-	def Evaluate(mapCmd[]):
-		return mapCmd[0]
-	pass
+
+    def Evaluate(self, mapCmd):
+        return mapCmd[0]
+
 
 class mapSum(ImapOper):
-	def Evaluate(mapCmd[]):
-		row_i = 2
-		for row in self.__stdList__(mapCmd)
-			value = 0
-			for col in row
-				value = col + value
-			sheet.cells(row_i).value = value
-		return
-	pass
+
+    def Evaluate(self, mapCmd):
+        row_i = 2
+        for row in self.__stdList__(mapCmd):
+            value = 0
+            for col in row
+                value = col + value
+                sheet.cells(row_i).value = value
