04ac50bdf5e0c149c5a0e00bb330ebecf4de6acf
Philip Wales <ptwales@gmail.com>
2014-02-10 12:06:19 -0600

    Update demo_01.py

--- DEMOS/demo_01.py
+++ DEMOS/demo_01.py
@@ -3,8 +3,10 @@
 fwp = open_workbook( 'fromSheet.xls' )
 twp = open_workbook( 'toSheet.xls' )
 
-cmCmd = sheetMapCmd ( { "A": [mapIs, "Hello, World"]
-                      , "B": [mapSum, "A", "B", "C"]
-                      , "C": [mapAss, "C", "D"]
-                      , "D": [mapProd, "B", "E"]
+cmCmd = sheetMapCmd ( { "A": colMapCmd( [mapIs, "Hello, World"] ) # map is accepts only 1 arg
+                      , "B": colMapCmd( [mapSum, "A", "B", "C"] )
+                      , "C": colMapCmd( [mapAss, "A", "C", "D"] )
+                      , "D": colMapCmd( [mapProd, colMapCmd( [mapSum, "A", "B"] ), "C", "D"] ) # stacking commands
                       } )
+
+interpColMap( cmCmd, fwp, twp, 1, 0, 1 )
