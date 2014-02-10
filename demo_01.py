625b93121529d4486c2cd4a50cdd1337346abd92
Philip Wales <ptwales@gmail.com>
2014-02-10 14:31:56 -0600

    Update demo_01.py

--- demo_01.py
+++ demo_01.py
@@ -1,12 +1,19 @@
 import colMapper
 
-fwp = open_workbook( 'fromSheet.xls' )
-twp = open_workbook( 'toSheet.xls' )
+fwb = open_workbook( 'fromSheet.xls' )
+fws = fwb.Sheets( 1 )
+twb = open_workbook( 'toSheet.xls' )
+twb = twb.Sheets( 1 )
 
-cmCmd = sheetMapCmd ( { "A": colMapCmd( [mapIs, "Hello, World"] ) # map is accepts only 1 arg
-                      , "B": colMapCmd( [mapSum, "A", "B", "C"] )
-                      , "C": colMapCmd( [mapAss, "A", "C", "D"] )
-                      , "D": colMapCmd( [mapProd, colMapCmd( [mapSum, "A", "B"] ), "C", "D"] ) # stacking commands
-                      } )
+demoCmd = MapCmd ( { "A": opList( (mapIs, "Hello, World") ) # map is accepts only 1 arg
+                   , "B": opList( (mapSum, "A", "B", "C") )
+                   , "C": opList( (mapAss, "A", "C", "D") )
+                   , "D": opList( (mapProd, opList( (mapSum, "A", "B") ), "C", "D") ) # stacking commands
+                 } )
 
-interpColMap( cmCmd, fwp, twp, 1, 0, 1 )
+interpColMap( demoCmd, fws, tws, 1, 0, 1 )
+
+# Pseudo Code
+fwb.close
+twb.save
+twb.close
