fd6c7f731560747239074e47961b534dd752573b
Philip Wales <ptwales@gmail.com>
2014-02-25 13:20:50 -0600

    WOOO NO CLASSES
    SCHOOLS OUT FOREVER!

--- demo_01.py
+++ demo_01.py
@@ -1,18 +1,20 @@
 from colMapper import *
+from collections import defaultdict
 import xlrd
 import xlwt
+
 fwb = xlrd.open_workbook('demo_1.xls')
 fws = fwb.sheet_by_index(0)
 twb = xlwt.Workbook()
 tws = twb.add_sheet('Sheet 1')
 
-demoCmd = MapCmd({
-    "A": opList([mapIs, "Hello, World"]),
-    "B": opList([mapSum, "A", "B", "C"]),
-    "C": opList([mapAss, "C", "D", "E"]),
-    "D": opList([mapProd, opList([mapSum, "B", "A"]), "A", "B"]),
-    "E": "F"
-})
+demoCmd = defaultdict(
+    "A": [mapIs, "Hello, World"],
+    "B": [mapSum, 1, 2, 3],
+    "C": [mapAss, 3, 4, 5],
+    "D": [mapProd, [mapSum, 2, 1], 1, 2],
+    "E": 6
+    )
 
 interpColMap(demoCmd, fws, tws, 1, 0, 1)
 twb.Save('out_demo_1.xls')
