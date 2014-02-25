e5132b829a4fc9deb7448b68bc8a2da073fee47f
Philip Wales <ptwales@gmail.com>
2014-02-25 12:09:59 -0600

    Columns must be integers
    xlwt only allows integer indexes and I agree. MapCmd keys can be strings but not opList args. Removed mapCmd class. Only use a plain dict.  Demo needs to be super updated.

--- colMapper/colMapper.py
+++ colMapper/colMapper.py
@@ -10,13 +10,44 @@ dict key is column to map to
 Total evaluation of a dict element contains the map to a column
 
 """
+EXCEL2010_MAXCOL = 16372 #is this in xlrd/xlwt?
+EXCEL2008_MAXCOL = 256
+MAX_COL = EXCEL2008_MAXCOL
 
+"""
+colAtoInt
+accepts a column index string in typical excel format 
+converts it to the appropriate integer.
 
-class opList(object):
+xlrd supplies colIndex to colname but not colname to colIndex
+Original Author
+https://stackoverflow.com/questions/763691/programming-riddle-how-might-you-translate-an-excel-column-name-to-a-number
+"""
+def colName_to_colIndex(colName):
+    index = 0
+    if type(colName) is str:
+        assert len(colName) <= 3
+        place = 1
+        for c in reversed(colName): #Original used [::-1]. I heard this was faster. Should test it
+            index += place*(int(c, 36) - 9)
+            place *= 26 # Grossest part is that this executes after we are done
+    elif type(colName) is int:
+        # nothing
+    else:
+        #raise error
+    assert index <= MAX_COL # check if larger than column count
+    return index
+    
+    
+
+class opList(list):
 
     def __init__(self, *cols):
         self._ = cols                # can be another colMapCmd
-
+        for obj in self._:
+            if type(obj) is int:
+                assert obj <= MAX_COL
+        
     def evaluate(self, fromSheet, row):
         if len(self._) == 1:
             return fromSheet.cell(row, self._.get(0)).value
@@ -25,32 +56,21 @@ class opList(object):
             for i in range(1, len(self)):
                 args[i] = self._.get(i).evaluate(fromSheet, row)
             return self._.get(0)(args)
+        
+        
 
+def MapCmdConvert(mapCmd):
+    for k in mapCmd.keys():
+        colIdx = colName_to_colIndex(k): # convert to integer
+        if k != colIdx:
+            mapCmd[colIdx] = mapCmd.pop(k)
+            
 
-class MapCmd(dict):
-
-    def __init__(self, dictColMapCmd):
-        self._ = dictColMapCmd
-        if not self._validate:
-            print("dictColMapCmd is invalid")
-            del self._
-
-    def _validate(self):
-        """
-        assert that every key be a key to a sheet column
-        therefore it must be either
-          a string of no more than 3 letters
-          or less than (max number of excel rows)
-
-        will fail if keys are not colMapCmd
-        """
-        return False
-
-
-def interpColMap(mpCmd, fmSheet, toSheet,
+def interpColMap(mapCmd, fmSheet, toSheet,
                  topRow=2, bottomRow=0, toTopRow=2):
     if bottomRow < 1:
         bottomRow = fmSheet.nrows
+    MapCmdConvert(mapCmd)
     assert topRow < bottomRow and toTopRow > 0
     for row in range(topRow, bottomRow):
         for key in list(mpCmd.keys()):
