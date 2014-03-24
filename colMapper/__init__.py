f96d4413855182b979a7ab9c0acc1da639e2eafb
Philip Wales <ptwales@gmail.com>
2014-03-24 11:41:46 -0500

    Added license

--- colMapper/__init__.py
+++ colMapper/__init__.py
@@ -1,1 +1,10 @@
-__all__ = ['colMapper', 'mapOper']
+# Copywrite (C) 2014 Philip Wales
+#
+#   This software is licensed under the GNU
+# Lesser General Public License (LGPL), version 3.
+# See the License for details about distribution rights,
+# and the specific rights regarding derivate works.
+#
+# You may obtain a copy of the License at:
+#
+# http://www.gnu.org/licenses/licenses.html
