fb39acc94b98574099d1ecf17c275605aa25062c
Philip Wales <ptwales@gmail.com>
2014-03-24 15:53:53 -0500

    Update xlsbmp.py

--- xlmp/xlsbmp.py
+++ xlmp/xlsbmp.py
@@ -1,5 +1,20 @@
 from . import xlmp
 
+
+def genSubSheetsByIds(fmSheet, idLocs, mapX=mapCols,
+                      fStart=0, fStop=-1):
+    if mapX:
+        M = xlmp.pullSheet(fmSheet, r0=fStart, rf=fStop)
+    else:
+        M = xlmp.__transpose(xlmp.pullSheet(fmSheet, c0=fStart, cf=fStop))
+    # sort M by idLocs
+    m = [[[None]]]
+    m_i = 0
+    for i in range(0, len(M)):
+        pass
+    return m
+
+
 # define functions to create subSheets, which will be matricies.
 def xlSubMapping(subCmd, subSheets, fSheet, tSheet, mapX=mapRow,
                  tStart=0):
