e807c9f145e23c5bd4dbf9fa5ada6fb8b17e2cc1
Philip Wales <ptwales@gmail.com>
2014-03-24 15:34:37 -0500

    Create xlsbmp.py

---
+++ xlmp/xlsbmp.py
@@ -0,0 +1,9 @@
+from . import xlmp
+
+# define functions to create subSheets, which will be matricies.
+def xlSubMapping(subCmd, subSheets, fSheet, tSheet, mapX=mapRow,
+                 tStart=0):
+    if mapX:
+        pass
+    else:
+        pass
