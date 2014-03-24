a7365d2a6b619bcad61859411d63a70187fa4b28
ptwales <ptwales@gmail.com>
2014-03-24 18:55:23 -0500

    finished xlsbmp
    Todo: remove __prealloc and have transposition rely on
    map_byCol_not_byRow

--- xlmp/xlmp.py
+++ xlmp/xlmp.py
@@ -1,36 +1,39 @@
 mapRow = False
 mapCol = True
+#map_byCol_not_byRow = True
+
 
 ##
 # Preallocation of Matrix M, assures that it is of the correct dimensions
+# probably poor form
 def __prealloc(X, Y):
     return [[None for y in range(Y)] for x in range(X)]
-    
-    
-### python-excel dependencies
 
+
+### python-excel dependencies
+### mapCol and mapRow should only be in these two
+### So it should read it transposed and write it transposed
 ##
 # ONLY XLRD Dependency
 # creates matrix of values of xlrd.Sheet object
 def pullSheet(s, r0=0, c0=0, rf=-1, cf=-1):
-    assert s.ragged_rows = False
-    if rf == -1:
-        rf = s.nrows
-    if cf == -1:
-        cf = s.ncols
-    assert r0 < rf and c0 < cf
-    xf = rf - r0
-    #yf = cf - c0
-    M = __prealloc(xf, cf - c0)
-    for x in range(xf):
-        M[x] = s.row_values(x - r0, c0, cf)
+    assert not s.ragged_rows
+    if rf < 0:
+        rf += s.nrows
+    if cf < 0:
+        cf += s.ncols
+    M = [[]]
+    for r in range(r0, rf):
+        M.append(s.row_values(r, c0, cf))
     return M
+
 
 ##
 # ONLY XLWT dependency
 # writes maxtrix M to xlwt.Sheet object
 # offset by r0 and c0
 def writeSheet(s, M, r0=0, c0=0):
+    assert r0 >= 0 and c0 >= 0
     for x in range(len(M)):
         for y in range(len(M[x])):
             s.write(r0 + x, c0 + y, M[x][y])
@@ -45,7 +48,7 @@ def writeSheet(s, M, r0=0, c0=0):
 # a blank row in column 2
 def __insertNullRows(M, D):
     assert len(M) == len(D)
-    B = __prealloc(max(D.keys()), len(M[0]))
+    B = __prealloc(max(D.keys()), len(M[0]))  # only place I should need __prealloc
     for r, k in zip(range(len(M)), D):
         B[k] = M[r]
     return B
@@ -70,21 +73,21 @@ def __mMap(M, F):
     X = len(M)
     Y = len(F)
     B = __prealloc(X, Y)
-    for r, i in zip(M, range(X)):
+    for r, i in zip(M, range(X)):  # should not need __prealloc here
         for f, j in zip(F, range(Y)):
             B[i][j] = __evalMapCmd(f, r)
     return B
 
 
 ##
-# M^T = B
+# transpose matrix M
 def __transpose(M):
     return zip(*M)
 
 
 def xlmap(Cmd, fSheet, tSheet, mapX=mapCol,
           fStart=0, fStop=-1, tStart=0):
-    if mapX:
+    if mapX:  # move this to pullSheet and writeSheet
         M = pullSheet(fSheet, r0=fStart, rf=fStop)
         #__ReplaceColNames(Cmd)
     else:
--- xlmp/xlsbmp.py
+++ xlmp/xlsbmp.py
@@ -1,24 +1,30 @@
 from . import xlmp
 
 
-def genSubSheetsByIds(fmSheet, idLocs, mapX=mapCols,
+def genSubSheetsByIds(fmSheet, idLocs, mapX=xlmp.mapCols,
                       fStart=0, fStop=-1):
     if mapX:
         M = xlmp.pullSheet(fmSheet, r0=fStart, rf=fStop)
     else:
         M = xlmp.__transpose(xlmp.pullSheet(fmSheet, c0=fStart, cf=fStop))
-    # sort M by idLocs
-    m = [[[None]]]
-    m_i = 0
-    for i in range(0, len(M)):
-        pass
+    for d in idLocs:
+        M.sort(key=lambda el: el[d])
+    m = [[M[0]]]
+    for r in M[1:]:
+        if all([m[-1][-1][d] == r[d] for d in idLocs]):
+            m[-1].append(r)
+        else:
+            m.append([r])
     return m
 
 
 # define functions to create subSheets, which will be matricies.
-def xlSubMapping(subCmd, subSheets, fSheet, tSheet, mapX=mapRow,
+def xlSubMapping(subCmd, subSheets, fSheet, tSheet, mapX=xlmp.mapRow,
                  tStart=0):
-    if mapX:
-        pass
-    else:
-        pass
+    for m in subSheets:
+        b = xlmp.__mMap(zip(*m), subCmd)
+        if mapX:
+            xlmp.writeSheet(tSheet, r0=tStart)
+        else:
+            xlmp.writeSheet(tSheet, c0=tStart)
+        tStart += len(b)
