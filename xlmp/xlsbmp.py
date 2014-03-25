87fa6be6fcaa70fd4a8972b97f43b4c92825eadd
Philip Wales <ptwales@gmail.com>
2014-03-25 13:41:46 -0500

    Carried over sub mapping changes
    I should probably push this back into xlmp.py

--- xlmp/xlsbmp.py
+++ xlmp/xlsbmp.py
@@ -1,12 +1,11 @@
 from . import xlmp
 
 
-def genSubSheetsByIds(fmSheet, idLocs, mapX=xlmp.mapCols,
-                      fStart=0, fStop=-1):
-    if mapX:
-        M = xlmp.pullSheet(fmSheet, r0=fStart, rf=fStop)
-    else:
-        M = xlmp.__transpose(xlmp.pullSheet(fmSheet, c0=fStart, cf=fStop))
+def genSubSheetsByIds(fmSheet, idLocs, mapX=xlmp.map_byCol_not_byRow,
+                      frng=(0, -1)):
+    global xlmp.map_byCol_not_byRow
+    xlmp.map_byCol_not_byRow = mapX
+    M = xlmp.pullSheet(fmSheet, xr=frng)
     for d in idLocs:
         M.sort(key=lambda el: el[d])
     m = [[M[0]]]
@@ -20,11 +19,8 @@ def genSubSheetsByIds(fmSheet, idLocs, mapX=xlmp.mapCols,
 
 # define functions to create subSheets, which will be matricies.
 def xlSubMapping(subCmd, subSheets, fSheet, tSheet, mapX=xlmp.mapRow,
-                 tStart=0):
+                 tp=(0,0)):
     for m in subSheets:
         b = xlmp.__mMap(zip(*m), subCmd)
-        if mapX:
-            xlmp.writeSheet(tSheet, r0=tStart)
-        else:
-            xlmp.writeSheet(tSheet, c0=tStart)
-        tStart += len(b)
+        xlmp.writeSheet(tSheet, tp)
+        tp[0] += len(b)
