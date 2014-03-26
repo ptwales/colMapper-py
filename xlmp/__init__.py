a680b08ff69af35a64c727169df5f1e93bcb9108
Philip Wales <ptwales@gmail.com>
2014-03-26 15:06:29 -0500

    fixed license

--- xlmp/__init__.py
+++ xlmp/__init__.py
@@ -1,10 +1,8 @@
 # Copywrite (C) 2014 Philip Wales
-#
-#   This software is licensed under the GNU
-# Lesser General Public License (LGPL), version 3.
+# This software is licensed under the GNU Lesser General 
+# Public License (LGPL), version 3 ("the License").
 # See the License for details about distribution rights,
 # and the specific rights regarding derivate works.
-#
 # You may obtain a copy of the License at:
-#
+ 
 # http://www.gnu.org/licenses/licenses.html
