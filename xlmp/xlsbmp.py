b38a20fbf2931d7ac6891c5b0afde3cd25f20d2f
Philip Wales <ptwales@gmail.com>
2014-03-31 14:46:46 -0500

    trying to un-mangle genSubSheetsByIds

--- xlmp/xlsbmp.py
+++ xlmp/xlsbmp.py
@@ -1,24 +1,29 @@
 from . import xlmp
 
 
-def genSubSheetsByIds(fmSheet, idLocs, mapX=xlmp.map_byCol_not_byRow,
+def genSubSheetsByIds(fmSheet, idLocs, mapX=xlmp.byCol_not_byRow,
                       frng=(0, -1)):
-    global xlmp.map_byCol_not_byRow
-    xlmp.map_byCol_not_byRow = mapX
+    global xlmp.byCol_not_byRow
+    xlmp.byCol_not_byRow = mapX
     M = xlmp.pullSheet(fmSheet, xr=frng)
     for d in idLocs:
         M.sort(key=lambda el: el[d])
+        
+    def __matchRowByIDs(ra, rb, idLocs):
+        return all([ra[d] == rb[d] for d in idLocs])
+        
     m = [[M[0]]]
     for r in M[1:]:
-        if all([m[-1][-1][d] == r[d] for d in idLocs]):
+        if __matchRowByIDs([m[-1][-1], r[d], idLocs):
             m[-1].append(r)
         else:
             m.append([r])
+    
     return m
 
 
 # define functions to create subSheets, which will be matricies.
-def xlSubMapping(subCmd, subSheets, fSheet, tSheet, mapX=xlmp.mapRow,
+def xlSubMapping(subCmd, subSheets, fSheet, tSheet, mapX=not xlmp.byCol_not_byRow,
                  tp=(0,0)):
     for m in subSheets:
         b = xlmp.__mMap(zip(*m), subCmd)
