332bf5acd9453ac5c0b4dae87a7563871f2d1683
Philip Wales <ptwales@gmail.com>
2014-04-03 19:48:14 -0500

    Extended xl interface, testing time

--- test/test_pullSheet.py
+++ test/test_pullSheet.py
@@ -1,46 +1,17 @@
-import xlwt
-import xlrd
 from xlmp import xlmp
 import string
+#import unittest
 
-
-def makeTestBook(bookName, sheetName):
-    book = xlwt.Workbook()
-    sheet = book.add_sheet(sheetName)
-    for r in range(0, 26):
-        for c in range(0, 26):
-            sheet.write(r, c, string.uppercase[c] + str(r + 1))
-    book.save(bookName)
-
+drng = list(range(0, 26))
 xlExts = ['.xls', '.xlsx', '.ods']
 testName = 'test'
-global map_byCol_not_byRow
+Mt = [[string.uppercase[c] + str(r + 1) for r in drng] for c in drng]
+global byCol_not_byRow
+
 
 if __name__ == '__main__':
     for x in xlExts:
-        xlmp.map_byCol_not_byRow = True
-        makeTestBook(testName + x, testName)
-        book = xlrd.open_workbook(testName + x)
-        sheet = book.sheet_by_index(0)
-
-        # test xlmp.pullsheet
-        M = xlmp.pullSheet(sheet)
-        print('Basic Pull' + '-' * 50)
-        print(M)
-
-        # test xlmp.writeSheet
-        cpyBk = xlwt.Workbook()
-        cpySh = cpyBk.add_sheet(testName)
-        xlmp.writeSheet(cpySh, M)
-        cpyBk.save(testName + x)
-
-        # test xlmp.map_byCol_not_byRow
-        xlmp.map_byCol_not_byRow = False
-        M = xlmp.pullSheet(sheet)
-        print('Transposed Pull' + '-' * 50)
-        print(M)
-        tnpsBk = xlwt.Workbook()
-        tnpsSh = tnpsBk.add_sheet(testName)
-        xlmp.writeSheet(tnpsSh, M)
-        tnpsBk.save('Trns' + testName + x)
-
+        testbook = testName + x
+        xlmp.byCol_not_byRow = True
+        xlmp.__writeBook(Mt, testbook, testName)
+        pass  # learn unittest
--- xlmp/xlmp.py
+++ xlmp/xlmp.py
@@ -1,26 +1,30 @@
-#mapRow = False
-#mapCol = True
+import xlrd
+import xlwt
+
 byCol_not_byRow = True
 
+###############################################################################
+#
+# XLRD XLWT Interfaces
+#
+###############################################################################
 
-### python-excel dependencies
+
 ##
-# ONLY XLRD Dependency
 # creates matrix of values of xlrd.Sheet object
-def pullSheet(s, xrng=(0, -1), yrng=(0, -1)):
+def __pullSheet(s, xrng=(0, -1), yrng=(0, -1)):
     assert not s.ragged_rows
     (crng, rrng) = (xrng, list(yrng)) if byCol_not_byRow else (yrng, list(xrng))
     if rrng[1] < 0:
         rrng[1] += s.nrows
-    M = [s.row_values(r, *crng for r in range(*rrng)
+    M = [s.row_values(r, *crng) for r in range(*rrng)]
     return (M if byCol_not_byRow else zip(*M))
 
 
 ##
-# ONLY XLWT dependency
 # writes maxtrix M to xlwt.Sheet object
 # offset by r0 and c0
-def writeSheet(s, M, p=(0, 0)):
+def __writeSheet(M, s, p=(0, 0)):
     for i in p:
         assert i >= 0
     if not byCol_not_byRow:
@@ -29,26 +33,58 @@ def writeSheet(s, M, p=(0, 0)):
     for x in range(len(M)):
         for y in range(len(M[x])):
             s.write(p[0] + x, p[1] + y, M[x][y])
-### END python-excel dependencies
+
+
+def __readBook(book_path, sheet_index=0, xrng=(0, -1), yrng=(0, -1)):
+    book = xlrd.open_workbook(book_path)
+    try:
+        sheet = book.sheet_by_index(sheet_index)
+    except TypeError:
+        sheet = book.sheet_by_name(sheet_index)
+    return __pullSheet(sheet, xrng, yrng)
+
+
+def __writeBook(M, book_path, sheet_name='xlmp', p=(0, 0)):
+    book = xlwt.Workbook()
+    sheet = book.add_sheet(sheet_name)
+    __writeSheet(M, sheet, p)
+
+
+###############################################################################
+#
+# Optional/User-Conveinece Interperters
+#  Removes zero offsets allow Excel Column names
+#
+###############################################################################
 
 
 ##
-# Converts all of the keys that are strings to 
+# Converts all of the keys that are strings to
 # integers, assuming that strings are xl colnames.
 # unnecessary function only for client side conviencence.
 def __ReplaceColNames(D):
     for k in list(D.keys()):
-        if type(k) is str:
+        if isinstance(k, str):
             place = 1
             index = 0
             for c in reversed(k):
                 index += place * (int(c, 36) - 9)
-                place *= 26  # Grossest part is that this executes after we are done
+                place *= 26
             index -= 1
         if k != index:
             D[index] = D.pop(k)
 
-    
+'''
+def __ReplaceZeroOffset(D):
+'''
+
+
+###############################################################################
+#
+# Data Mapping
+#
+###############################################################################
+
 
 ##
 # M might be a smaller matrix than the range on the sheet
@@ -72,8 +108,8 @@ def __insertNullCols(M, D):
 def __evalMapCmd(f, r):
     if isinstance(f, str):
         return f
-    elif isinstance(f, (int, long)):
-        return r[f] # if zeroOffset else r[f-1]
+    elif isinstance(f, int):
+        return r[f]  # if zeroOffset else r[f-1] // that should be done before
     else:  # this looks like horrible recursion
         return f[0](*[__evalMapCmd(a, r) for a in f[1]])
 
@@ -84,21 +120,30 @@ def __evalMapCmd(f, r):
 # default is by cols of F and rows of M
 # M must be transposed beforehand for other method
 def __mMap(M, F):
-    B = __insertNullCols([__evalMapCmd(f, m) for f in F] for m in M], F)
+    return __insertNullCols([[__evalMapCmd(f, m) for f in F] for m in M], F)
+
+
+###############################################################################
+#
+# User Accessable Functions
+#
+###############################################################################
 
 
 ##
-# Maps from fSheet to tSheet according to Cmd
-def xlmap(Cmd, fSheet, tSheet, mapX=byCol_not_byRow,
-          frng=(0, -1), tp=(0, 0)):
+# Maps from fBook to tBook according to Cmd
+def xlmap(Cmd, fBook='', tBook='', fSheet=0, tSheet='xlmp',
+          mapX=byCol_not_byRow, frng=(0, -1), tp=(0, 0)):
     global byCol_not_byRow
     byCol_not_byRow = mapX
     if mapX:
         __ReplaceColNames(Cmd)
-    writeSheet(tSheet, 
-               __mMap(pullSheet(fSheet, frng), 
-                      [Cmd[k] for k in Cmd]),
-               tp)
-    # M = pullSheet(fSheet, frng)
-    # M = __mMap(M, [Cmd[k] for k in Cmd])
-    # writeSheet(tSheet, M, tp)
+    try:
+        M = __pullSheet(fSheet, frng)
+    except AttributeError:
+        M = __readBook(fBook, fSheet, frng)
+    M = __mMap(M, [Cmd[k] for k in Cmd])
+    try:
+        __writeSheet(M, tSheet, tp)
+    except AttributeError:
+        __writeBook(M, tBook, tSheet, tp)
