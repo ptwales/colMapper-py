3bc3d9bd242cc5865e3cbb44d9fd3241cd7c7392
Philip Wales <ptwales@gmail.com>
2014-04-11 16:10:26 -0500

    Update and rename test_pullSheet.py to test_xlrw.py

--- test/test_xlrw.py
+++ test/test_xlrw.py
@@ -1,4 +1,4 @@
-from xlmp import xlmp
+import xlmp
 import string
 #import unittest
 
@@ -6,7 +6,6 @@ drng = list(range(0, 26))
 xlExts = ['.xls', '.xlsx', '.ods']
 testName = 'test'
 Mt = [[string.uppercase[c] + str(r + 1) for r in drng] for c in drng]
-global byCol_not_byRow
 
 
 if __name__ == '__main__':
