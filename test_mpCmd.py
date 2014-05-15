1108a42af9eff934b24e19959e67a63a08638e71
Philip Wales <ptwales@gmail.com>
2014-05-15 17:55:29 -0500

    test_func_vak_replace passed

--- test_mpCmd.py
+++ test_mpCmd.py
@@ -25,28 +25,28 @@ class TestmpCmd(unittest.TestCase):
     #
     # stupid test case; nothing should change
     # index -> index
-    def index_key(self):
+    def test_index_key(self):
         cmd = xlmp.mpCmd(i_INDEX_COMMAND)
         self.assertEqual(cmd.keys(), o_INDEX_COMMAND.keys())
 
     # index -> index - offset
-    def index_key_offset(self):
+    def test_index_key_offset(self):
         cmd = xlmp.mpCmd(i_OFFSET_COMMAND, offset=1)
         self.assertEqual(cmd.keys(), o_INDEX_COMMAND.keys())
 
     # name -> index
-    def name_key_replace(self):
+    def test_name_key_replace(self):
         cmd = xlmp.mpCmd(i_NAME_COMMAND)
         self.assertEqual(cmd.keys(), o_INDEX_COMMAND.keys())
 
     # there is no name offset
     # name -> index
-    def name_key_offset(self):
+    def test_name_key_offset(self):
         cmd = xlmp.mpCmd(i_NAME_COMMAND, offset=1)
         self.assertEqual(cmd.keys(), o_INDEX_COMMAND.keys())
 
     # keys can only be int or str
-    def invalid_key(self):
+    def test_invalid_key(self):
         with self.assertRaises(TypeError):
             cmd = xlmp.mpCmd({[1, 2, 3]: 4})
             cmd = xlmp.mpCmd({2.0: 4})
@@ -56,15 +56,15 @@ class TestmpCmd(unittest.TestCase):
     #
     # index -> (lambda row: row[index])
     #
-    def index_val_replace(self):
-        cmd = xlmp.mpCmd(i_INDEX_COMMAND, int_is_name=True)
+    def test_index_val_replace(self):
+        cmd = xlmp.mpCmd(i_INDEX_COMMAND, int_is_index=True)
         self.assertEqual(
             [cmd[k](TEST_ROW) for k in cmd.keys()],
             [o_INDEX_COMMAND[k](TEST_ROW) for k in o_INDEX_COMMAND.keys()]
             )
 
     # name -> (lambda row: row[index])
-    def name_val_replace(self):
+    def test_name_val_replace(self):
         cmd = xlmp.mpCmd(i_NAME_COMMAND, str_is_name=True)
         self.assertEqual(
             [cmd[k](TEST_ROW) for k in cmd.keys()],
@@ -72,7 +72,7 @@ class TestmpCmd(unittest.TestCase):
             )
 
     # val -> (lambda *args, **kwargs: val)
-    def value_val_replace(self):
+    def test_value_val_replace(self):
         cmd = xlmp.mpCmd({0: TEST_STRING, 1: 1},
                          str_is_name=False, int_is_index=False)
         self.assertEqual(cmd[0](TEST_ROW), TEST_STRING)
@@ -80,10 +80,13 @@ class TestmpCmd(unittest.TestCase):
 
 
     # (func, (indexes)) -> (lambda row: func(*rmap((lambda i: row[i]), indexes)
-    def func_val_replace(self):
-        cmd = xlmp.mpCmd({0: (sum, ([0, 2, 4], 0))}, int_is_index=True) 
-        self.assertEqual(cmd[0]([1, -1600, 5000, 4999]), 9001)
-        #cmd = xlmp.mpCmd({0, (''.join, ([18))
+    def test_func_val_replace(self):
+        cmd = xlmp.mpCmd({
+            0: (sum, ([0, 2, 4], 0)),
+            1: (''.join, [[19,7,8,18,8,18,0,13,4,23,15,0,17,17,14,19]])
+            }, int_is_index=True) 
+        self.assertEqual(cmd[0]([1, -1600, 4999, -5000, 4000]), 9001)
+        self.assertEqual(cmd[1](string.ascii_uppercase), "THISISANEXPARROT")
 
 
 if __name__ == '__main__':
