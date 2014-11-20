92c9cc56e8b80e5b67e0cd06ae4ed57188b75b66
Philip Wales <ptwales@gmail.com>
2014-11-20 09:54:01 -0600

    Restructured factory method.

--- xlmp/SheetIO.py
+++ xlmp/SheetIO.py
@@ -1,38 +1,39 @@
 import xlrd
 import xlwt
+import csv
 
-class ISheetIO(object):
-    """ Abstract base class for SheetIO
+
+def factory_create_sheet(sheet):
+    """ Factory Method, Decide which concrete
+    class to return based on the sheet type.
     """
-    def read_sheet(self):
-        raise NotImplementedError("read_sheet not implemented")
-    
-    def write_sheet(self):
-        raise NotImplementedError("write_sheet not implemented")
-    
-    
-    
-class SheetIO(ISheetIO):
-  
+    factory_registry = {
+        xlrd.sheet: ExcelSheetIO(),
+        xlwt.WorkSheet: ExcelSheetIO(),
+        csv.reader: CsvSheetIO(),
+        csv.writer: CsvSheetIO()
+    }
+    try:
+        return factory_registry[type(sheet)]
+    except(KeyError):
+        raise TypeError("Unknown Sheet type or not a sheet")
+
+
+
+class UniversalSheetIO(object):
+    """ Universal Reader that implements factory method and
+    matrix transposition.
+    """
     def __init__(self, read_by_row=True):
         self.by_row = read_by_row
-      
-    def _factory(self, sheet):
-        sheet_type = type(sheet)
-        if sheet_type is xlrd.sheet or sheet_type is xlwt.WorkSheet:
-            return ExcelSheet(read_by_row)
-        # elif CSV
-        # elif OpenXML
-        else:
-            raise TypeError("Unknown Sheet type or not a sheet")
-    
+        
     def read_sheet(self, sheet, **kwargs):
-        data = self._factory(sheet).read_sheet(sheet, **kwargs)
+        data = factory_create_sheet(sheet).read_sheet(sheet, **kwargs)
         return self.orient_data(data)
     
     def write_sheet(self, data, sheet, **kwargs):
         oriented_data = self._orient_data(data)
-        self._factory(sheet).write_sheet(sheet, oriented_data, **kwargs)
+        factory_create_sheet(sheet).write_sheet(sheet, oriented_data, **kwargs)
     
     def _orient_data(self, data):
         if self.by_row:
@@ -45,19 +46,29 @@ class SheetIO(ISheetIO):
 
 
 
-class ExcelSheet(ISheetIO):
+class ConcreteSheetIO(object):
+    """ Abstract base class for SheetIO
+    """
+    def read_sheet(self):
+        raise NotImplementedError("read_sheet not implemented")
+    
+    def write_sheet(self):
+        raise NotImplementedError("write_sheet not implemented")
 
+
+
+class ExcelSheetIO(ConcreteSheetIO):
+    """ Concrete implementation of Excel Sheets using xlrd and xlwt
+    """
     def read_sheet(self, sheet,
             col_start=0, row_start=0, col_cnt=-1, row_cnt=-1):
-        
         # TODO: Raise an error instead. Research which error to raise
         assert not sheet.ragged_rows
-        
+        # Allow wrapping
         if row_cnt < 0:
             row_cnt += sheet.nrows + 1
         if col_cnt < 0:
             col_cnt += sheet.ncols + 1
-            
         return [sheet.row_values(r, col_start, row_start) 
                 for r in xrange(row_start, row_cnt + 1)]
 
@@ -68,12 +79,12 @@ class ExcelSheet(ISheetIO):
 
 
 
-class CsvSheet(ISheetIO):
-
+class CsvSheetIO(ConcreteSheetIO):
+    """ Concreted implementation using csv module
+    """
     def read_sheet(self, csv_reader, 
             col_start=0, row_start=0, col_cnt=-1, row_cnt=-1):
-        return [row[col_start:col_cnt] for row in 
-                csv_reader][row_start:row_cnt]
+        return [row[col_start:col_cnt] for row in sv_reader][row_start:row_cnt]
 
     def write_sheet(self, data, csv_writer):
         csv_writer.writerows(data)
